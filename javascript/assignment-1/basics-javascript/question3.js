3ffed3cca53183cbd3bd81fe8cea54df0816dbfe
kkritii <kritiipraz@gmail.com>
2022-01-04 20:32:04 +0545

    Add Javascript Assignment 1 - Basics of Javascript

--- javascript/assignment-1/basics-javascript/question3.js
+++ javascript/assignment-1/basics-javascript/question3.js
@@ -1,10 +1,16 @@
+// Question 3
+// Converting Objects to Arrays
+// Write a function that converts an object into an array, where each element represents a key-value pair.
+
 function ObjectToArray(obj) {
     let objKeysArray = Object.keys(obj);
     let result = [];
-    objKeysArray.forEach(function(key) {
-        result.push([key, obj[key]]);
-    });
+    for(let i=0; i<objKeysArray.length; i++) {
+        result.push([objKeysArray[i], obj[objKeysArray[i]]]);
+    }
     return result;
 }
 
 console.log(ObjectToArray({ a: 1, b: 2 }));
+console.log(ObjectToArray({ shrimp: 15, tots: 12 }));
+console.log(ObjectToArray({}));
