1489af4c3bf2d3377a7430730b6c72d6dbab1cec
kkritii <kritiipraz@gmail.com>
2022-01-08 14:39:36 +0545

    Add string-manipulation assignment

---
+++ javascript/assignment-1/string-manipulation/question1.js
@@ -0,0 +1,7 @@
+function giveMeSomething(str) {
+    return("something " + str);
+}
+
+console.log(giveMeSomething("is better than nothing"));
+console.log(giveMeSomething("Bob Jane"));
+console.log(giveMeSomething("something"));
---
+++ javascript/assignment-1/string-manipulation/question2.js
@@ -0,0 +1,7 @@
+function newWord(word) {
+    return (word.substring(1));
+}
+
+console.log(newWord("apple"));
+console.log(newWord("cherry"));
+console.log(newWord("plum"));
