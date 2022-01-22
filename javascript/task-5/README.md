81414530d1f4924ae5fa8c5e6df9e5b815003c08
kkritii <kritiipraz@gmail.com>
2022-01-23 01:27:50 +0545

    Add JS Task 4 - Form Validation

--- javascript/task-5/README.md
+++ javascript/task-5/README.md
@@ -3,4 +3,3 @@
 **Form Validator**  
 ### [**`Demo Here`**](https://coderushnepal.github.io/KritiPrajapati/javascript/task-5/)
 
-<img src="image/demo.PNG/" width="500px">
--- javascript/task-5/js/script.js
+++ javascript/task-5/js/script.js
@@ -1,8 +1,10 @@
 var body = document.body;
 var mainWrapper = document.createElement('div');
 body.appendChild(mainWrapper);
+
 var container = document.createElement('div');
 container.classList.add("container");
+
 var form = document.createElement('form');
 form.classList.add("form");
 form.id = "form";
@@ -10,26 +12,24 @@ mainWrapper.appendChild(container);
 container.appendChild(form);
 
 mainWrapper.style = `
-padding-top: 100px;
+    padding-top: 90px;
     min-width: 700px;
     font-size: 16px;
     font-family:  'Open Sans', sans-serif;
     background-color: #f9fafb;
-`
+`;
 
 container.style = ` 
-width: 400px;
-margin: 0 auto;
-border: 1px solid #868686;
-background-color: #ffffff;
-border-radius: 5px;
-box-shadow: 0 2px 10px rgb(0 0 0 / 30%);
+    width: 400px;
+    margin: 0 auto;
+    background-color: #ffffff;
+    border-radius: 5px;
+    box-shadow: 0 2px 10px rgb(0 0 0 / 30%);
 `;
 
 form.style = `
-padding: 30px 40px;
+    padding: 30px 40px;
 `;
-
 
 var titleForm = document.createElement("h2");
 titleForm.innerHTML = "Register With Us";
@@ -53,19 +53,16 @@ var formControls = [{
     type: "email",
     id: "email",
     placeholder: "Enter email",
-    errorMessage: "Email is not valid"
 }, {
     label: "Password",
     type: "password",
     id: "password",
     placeholder: "Enter password",
-    errorMessage: "Password must be at least 6 characters"
 }, {
     label: "Confirm Password",
     type: "password",
     id: "password2",
-    placeholder: "Enter password again",
-    errorMessage: "Password2 is required"
+    placeholder: "Enter password again"
 }];
 
 formControls.forEach(function(value) {
@@ -82,7 +79,6 @@ formControls.forEach(function(value) {
 
     var smallDispaly = document.createElement('small');
     smallDispaly.id = "error-" + value.id;
-    smallDispaly.innerHTML = value.errorMessage;
 
     form.appendChild(formControl);
     formControl.appendChild(titleLabel);
@@ -99,37 +95,28 @@ formControls.forEach(function(value) {
 
     //css titleLabel
     titleLabel.style = `
-    color: #777;
-    display: block;
-    margin-bottom: 5px;
-    `
+        color: #777;
+        display: block;
+        margin-bottom: 5px;
+    `;
 
     inputField.style = `
-    width: 100%;
-    border: 2px solid #f0f0f0;
-    border-radius: 4px;
-    display: block;
-    width: 100%;
-    padding: 10px;
-    font-size: 14px;
+        width: 100%;
+        border: 2px solid #f0f0f0;
+        border-radius: 4px;
+        width: 92.5%;
+        padding: 10px;
+        font-size: 14px;
     `;
 
     smallDispaly.style = `
-    color: #e74c3c;
-    position: absolute;
-    bottom: 0;
-    left: 0;
+        color: #e74c3c;
+        position: absolute;
+        bottom: 0;
+        left: 0;
     `;
     smallDispaly.style.visibility = "hidden";
-
 });
-
-
-
-
-
-
-
 
 
 
@@ -142,18 +129,15 @@ form.appendChild(btnSubmit);
 
 //css btn
 btnSubmit.style = `
-    border: 0;
-    border-radius: 5px;
-    background-color: #00B000;
-    color: white;
-    font-size: 16px;
-    font-weight: 600;
-    padding: 12px 22px;
     cursor: pointer;
-    margin-bottom: 10px;
-`
-
-
+    background-color: #3498db;
+    border: 2px solid #3498db;
+    border-radius: 4px;
+    color: #fff;
+    font-size: 16px;
+    padding: 10px;
+    width: 100%;
+`;
 
 btnSubmit.addEventListener("click", function(event) {
     event.preventDefault();
@@ -165,52 +149,72 @@ btnSubmit.addEventListener("click", function(event) {
     var errorEmail = document.getElementById('error-email');
     var errorPassword = document.getElementById('error-password');
     var errorPassword2 = document.getElementById('error-password2');
-    var mailFormat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
 
-
-
-    if (username.value === "") {
-        errorUsername.style.visibility = "visible";
-        errorUsername.innerHTML = "Username is required ";
-    } else if (username.value.length < 3) {
-        errorUsername.style.visibility = "visible";
-        errorUsername.innerHTML = "Username must be at least 3 characters ";
-    } else {
+    //username validation
+    if (requiredValidator(username, errorUsername) && lengthValidator(username, errorUsername, 3)) {
         username.style.borderColor = '#2ecc71';
         errorUsername.style.visibility = "hidden";
     }
 
-    if (email.value === "") {
-        errorEmail.style.visibility = "visible";
-        errorEmail.innerHTML = "Email is required ";
-    } else if (!mailFormat.test(email.value)) {
-        errorEmail.style.visibility = "visible";
-        errorEmail.innerHTML = "Email is invalid";
-    } else {
+    //email validation
+    if (requiredValidator(email, errorEmail) && emailValidator(email, errorEmail)) {
         email.style.borderColor = '#2ecc71';
         errorEmail.style.visibility = "hidden";
     }
 
-    if (password.value === "") {
-        errorPassword.style.visibility = "visible";
-        errorPassword.innerHTML = "Password is required ";
-    } else if (password.value.length < 6) {
-        errorPassword.style.visibility = "visible";
-        errorPassword.innerHTML = "Password must be at least 6 characters";
-    } else {
+    //password validatiom
+    if (requiredValidator(password, errorPassword) && lengthValidator(password, errorPassword, 6)) {
         password.style.borderColor = '#2ecc71';
         errorPassword.style.visibility = "hidden";
     }
 
-    if (password2.value !== password.value) {
-        errorPassword2.style.visibility = "visible";
-        errorPassword2.innerHTML = "Passwords do not match";
-    } else if (password2.value === "") {
-        errorPassword2.style.visibility = "visible";
-        errorPassword2.innerHTML = "Password2 is required";
-    } else {
+    //password confirmation
+    if (confirmPasswordValidaor(password, password2, errorPassword2) && requiredValidator(password2, errorPassword2)) {
         password2.style.borderColor = '#2ecc71';
         errorPassword2.style.visibility = "hidden";
     }
 
 });
+
+
+function requiredValidator(inputField, errorHolder) {
+    if (inputField.value === "") {
+        errorHolder.style.visibility = "visible";
+        errorHolder.innerHTML = `${inputField.id} is required `;
+        inputField.style.borderColor = 'red';
+        return false;
+    }
+    return true;
+}
+
+function lengthValidator(inputField, errorHolder, length) {
+    if (inputField.value.length < length) {
+        errorHolder.style.visibility = "visible";
+        errorHolder.innerHTML = `${inputField.id} must be at least ${length} characters`;
+        inputField.style.borderColor = 'red';
+        return false;
+    }
+    return true;
+}
+
+function emailValidator(inputField, errorHolder) {
+    var mailFormat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+
+    if (!mailFormat.test(inputField.value)) {
+        errorHolder.style.visibility = "visible";
+        errorHolder.innerHTML = "Email is invalid";
+        inputField.style.borderColor = 'red';
+        return false;
+    }
+    return true;
+}
+
+function confirmPasswordValidaor(password, password2, errorHolder) {
+    if (password2.value !== password.value) {
+        errorHolder.style.visibility = "visible";
+        errorHolder.innerHTML = "Passwords do not match";
+        password2.style.borderColor = 'red';
+        return false;
+    }
+    return true;
+}
