4c28d9b6a4aa4a84ca49aaa4dccdeda4f12cc5d5
kkritii <kritiipraz@gmail.com>
2022-05-18 10:48:33 +0545

    updated: selected ids of both owner and donar in sql

--- finalproject/server/src/db/queries/getPostDetails.js
+++ finalproject/server/src/db/queries/getPostDetails.js
@@ -11,13 +11,11 @@ post.collected_amount,
 post.category,
 post.start_date,
 post.end_date,
-json_agg(json_build_object('id', donation.id, 'name',donation.full_name,'amount',donation.amount, 'donar_avatar', donation.avatar, 'message',donation.message, 'donated_at', donation.donated_at)) AS donar_detail,
+json_agg(json_build_object('id', donation.id,'donar_id',donation.donar_user_id, 'name',donation.full_name,'amount',donation.amount, 'donar_avatar', donation.avatar, 'message',donation.message, 'donated_at', donation.donated_at)) AS donar_detail,
 json_agg(json_build_object('id', updates.id, 'message',updates.message, 'updated_at', updates.updated_at)) AS post_updates
 
-
 FROM (SELECT p.id,p.post_title, p.post_description,p.owner_user_id,p.report_count, p.end_date,u.full_name as owner_name, u.avatar as owner_avatar,  p.target_amount,p.collected_amount, p.category, p.start_date FROM posts p
-
-INNER JOIN users u on p.owner_user_id = u.id ) AS post
+    INNER JOIN users u on p.owner_user_id = u.id ) AS post
 
 LEFT JOIN (SELECT d.post_id,d.donar_user_id,d.amount,d.message,d.donated_at,d.id, ud.full_name, ud.avatar FROM donations d
     INNER JOIN users ud on d.donar_user_id = ud.id) AS donation
