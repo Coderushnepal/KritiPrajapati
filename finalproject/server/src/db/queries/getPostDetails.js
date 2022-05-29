84fddee3876ca60b8ef9f829a21ce1e80c5d8e62
kkritii <kritiipraz@gmail.com>
2022-05-29 11:14:58 +0545

    bug fix: filtered null rows from table for singlepost

--- finalproject/server/src/db/queries/getPostDetails.js
+++ finalproject/server/src/db/queries/getPostDetails.js
@@ -12,9 +12,8 @@ post.collected_amount,
 post.category,
 post.start_date,
 post.end_date,
-json_agg(distinct jsonb_build_object('id', donation.id, 'donar_id',donation.donar_user_id, 'name',donation.full_name,'amount',donation.amount, 'donar_avatar', donation.avatar, 'message',donation.message, 'donated_at', donation.donated_at)) AS donar_detail,
-json_agg(distinct jsonb_build_object('id', updates.id, 'message',updates.message, 'updated_at', updates.updated_at)) AS post_updates
-
+json_agg(distinct jsonb_build_object('id', donation.id, 'donar_id',donation.donar_user_id, 'name',donation.full_name,'amount',donation.amount, 'donar_avatar', donation.avatar, 'message',donation.message, 'donated_at', donation.donated_at)) filter (where donation.id is not null) AS donar_detail ,
+json_agg(distinct jsonb_build_object('id', updates.id, 'message',updates.message, 'updated_at', updates.updated_at)) filter (where updates.id is not null) AS post_updates
 
 FROM (SELECT p.id,p.post_title, p.post_description,p.owner_user_id,p.report_count, p.end_date,u.full_name as owner_name, u.avatar as owner_avatar,  p.target_amount,p.collected_amount, p.category, p.start_date FROM posts p
     INNER JOIN users u on p.owner_user_id = u.id ) AS post
